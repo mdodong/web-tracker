1124c39e0546469b78b7ae76e4d0d25393e2dee8
Mark John Tiempo <mdodong@gmail.com>
2015-07-04 21:58:48 +0800

    added config file

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php
+/* 
+* Database and other configuration settings
+*/
+
+/*** mysql hostname ***/
+define('DB_HOST','localhost');
+
+/*** mysql username ***/
+define('DB_USER','hybrid-admin');
+
+/*** mysql password ***/
+define('DB_PASSWORD','Hybrid-@dmin1!');
+
+/*** mysql database name ***/
+define('DB_NAME','tracker-hybrid');
+
+/*** mysql DSN ***/
+define('DB_DSN',"mysql:host=".DB_HOST .";dbname=" .DB_NAME);
+
+define( "CLASS_PATH", "classes" );
+define( "TEMPLATE_PATH", "templates" );
+
+date_default_timezone_set('America/Chicago');
+
+require( CLASS_PATH . "/class.Caselog.php" );
+
+
+
+///Colors
+$colors[]      = 'background-color: #65CC35; color: #FFF;';
+$colors[] = 'background-color: #47A2F2; color: #FFF;';
+$colors[]     = 'background-color: #FFDA00; color: #FFF;';
+$colors[]        = 'background-color: #FF3871; color: #FFF;';
+$colors[]    = 'background-color: #337AB7; color: #FFF;';
+
+
+?>
